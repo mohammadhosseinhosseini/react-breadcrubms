bf5da82c44d51c9249e9374e5be1b5c59e8561c6
mohammadhossein211 <mohammadhossein211@gmail.com>
2022-09-14 11:52:29 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import Breadcrumb from 'react-bootstrap/Breadcrumb'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [breadcrumbs, setBreadcrubms] = useState([])
+    useEffect(() => {
+        // Fetch API here ->
+        setBreadcrubms([
+            {
+                name: 'some-page-1',
+                link: '/some/path/1',
+                title: 'Link title for page 1',
+                label: 'Some page 1',
+            },
+            {
+                name: 'some-page-1',
+                link: '/some/path/2',
+                title: 'Link title for page 1',
+                label: 'Some page 2',
+            },
+        ])
+    }, [])
+
+    return (
+        <div className='container mt-5'>
+            {breadcrumbs.length > 0 && (
+                <Breadcrumb>
+                    <Breadcrumb.Item
+                        href='example.com' // Home page link
+                    >
+                        Home
+                    </Breadcrumb.Item>
+                    {breadcrumbs.map((b, index) => (
+                        <Breadcrumb.Item
+                            key={index}
+                            href={b.link}
+                            active={index + 1 == breadcrumbs.length}
+                        >
+                            {b.label}
+                        </Breadcrumb.Item>
+                    ))}
+                </Breadcrumb>
+            )}
+        </div>
+    )
 }
 
-export default App;
+export default App
